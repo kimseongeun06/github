76e822058a26fc297fb3da1fd0e10455064d5091
kimseongeun06 <teresa119@naver.com>
2021-05-13 03:24:46 +0900

    Add java File [Project.java]

--- Project.java
+++ Project.java
@@ -2,7 +2,12 @@ package hello;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -15,7 +20,16 @@ import javax.swing.JScrollPane;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.awt.ScrollPane;
+import java.awt.FileDialog;
+import javax.swing.JOptionPane;
+import java.awt.event.*;
+import java.awt.Frame;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.awt.datatransfer.*;
+import java.awt.Toolkit;
 
 public class Project extends JFrame {
 
@@ -34,12 +48,13 @@ public class Project extends JFrame {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-			}
-		});
-	}
+			}	
+		});	
+	}	
 
 String copyText;
 	public Project() {
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 532, 408);
 		
@@ -77,16 +92,62 @@ String copyText;
 		
 		JMenuItem mntmNewMenuItem_1 = new JMenuItem("New");
 		mntmNewMenuItem_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			
+			public void actionPerformed(ActionEvent e) {
+				textArea.setText("");
 			}
 		});
 		mnNewMenu.add(mntmNewMenuItem_1);
 		
+
 		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Open");
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fs = new JFileChooser(new File("C:\\"));
+				fs.setDialogTitle("Open a file");
+				int result = fs.showSaveDialog(null);
+				if(result == JFileChooser.APPROVE_OPTION) {
+					try {
+						File fi = fs.getSelectedFile();
+						BufferedReader br = new BufferedReader(new FileReader(fi.getPath()));
+						String line = "";
+						String s = "";
+						while((s = br.readLine()) != null) {     
+							line += s;    
+						}
+						textArea.setText(line);
+						if(br != null)
+							br.close();
+	
+					} catch (Exception e2) {
+						JOptionPane.showMessageDialog(null, e2.getMessage());
+					}
+				}		
+			}
+		});
+	
 		mnNewMenu.add(mntmNewMenuItem_2);
 		
 		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Save");
+		mntmNewMenuItem_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fs = new JFileChooser(new File("C:\\"));
+				fs.setDialogTitle("Save a file");
+				int result = fs.showSaveDialog(null);
+				if(result == JFileChooser.APPROVE_OPTION) {
+					String content = textArea.getText();
+					File fi = fs.getSelectedFile();
+					try {
+						FileWriter fw = new FileWriter(fi.getPath() + ".java");
+						fw.write(content);
+						fw.flush();
+						fw.close();
+						
+					} catch(Exception e2) {
+						JOptionPane.showMessageDialog(null, e2.getMessage());
+					}
+				}	
+			}
+		});
 		mnNewMenu.add(mntmNewMenuItem_3);
 		
 		JMenuItem mntmNewMenuItem = new JMenuItem("Exit");
@@ -105,9 +166,9 @@ String copyText;
 		mnNewMenu_1.add(mntmNewMenuItem_5);
 		mntmNewMenuItem_5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == mntmNewMenuItem_5) {
-					copyText = textArea.getSelectedText();
-				}
+				Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+	            StringSelection contents = new StringSelection(textArea.getText());
+	            cb.setContents(contents, null);
 			}
 		});
 		
@@ -116,11 +177,14 @@ String copyText;
 		mnNewMenu_1.add(mntmNewMenuItem_6);
 		mntmNewMenuItem_6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == mntmNewMenuItem_6) {
-					int position = textArea.getCaretPosition();
-					textArea.insert(copyText, position);
-				}
-						
+				Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+	            Transferable contents = cb.getContents(cb);
+	            if(contents != null) {
+	               try {
+	                String pasteString = (String)(contents.getTransferData(DataFlavor.stringFlavor));
+	                   textArea.insert(pasteString, textArea.getCaretPosition());
+	               }catch (Exception k) {}
+	            }
 			}
 		});
 		
@@ -128,24 +192,53 @@ String copyText;
 		mnNewMenu_1.add(mntmNewMenuItem_4);
 		mntmNewMenuItem_4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == mntmNewMenuItem_4) {
-					copyText = textArea.getSelectedText();
-					int start = textArea.getSelectionStart();
-					int end = textArea.getSelectionEnd();
-					textArea.replaceRange("", start, end);
-				}
+				Clipboard cb = Toolkit.getDefaultToolkit().getSystemClipboard();
+	            StringSelection contents = new StringSelection(textArea.getText());
+	            cb.setContents(contents, null);
+	            textArea.setText(null);
 			}
 		});
-		
+
 		JMenu mnNewMenu_2 = new JMenu("Compile");
 		menuBar.add(mnNewMenu_2);
 		
+		Cmd cmd = new Cmd();
 		JMenuItem mntmNewMenuItem_8 = new JMenuItem("Compile");
+		mntmNewMenuItem_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = null;
+				name = JOptionPane.showInputDialog("파일이름을 적어주세요.");
+	            cmd.FILENAME = name+".java";
+				String[] code = textArea.getText().split("\n");
+				StringBuffer buffer = new StringBuffer();
+				for (int i = 0; i<code.length; i++) {
+		               buffer.append(code[i]);
+		            }
+				String command = cmd.inputSource(buffer.toString());
+				String result = cmd.execCommand(command);
+				
+				textArea_1.append(result);
+			
+			}
+		});
 		mnNewMenu_2.add(mntmNewMenuItem_8);
 		
+		
 		JMenuItem mntmNewMenuItem_7 = new JMenuItem("Run");
+		mntmNewMenuItem_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String result = cmd.Run();
+	            textArea_1.append(result);
+				
+			}
+		});	
 		mnNewMenu_2.add(mntmNewMenuItem_7);
 		
 	}
-
+	
 }
+
+		
+		
+	
+
